2d5977dafafefdada21616264f40ee4d94163caa
Rohit Tokala <rtokala@purdue.edu>
2022-11-15 17:13:21 -0500

    Initialize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <h1>Initial</h1>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/new" element={<h1>New</h1>}></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
