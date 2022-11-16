7a87b3441367b55e91538432f9bd33dd8661d2ce
Rohit Tokala <rtokala@purdue.edu>
2022-11-15 19:19:36 -0500

    Add markdown to show page

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,4 +1,5 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { Link } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 
@@ -32,6 +33,7 @@ export function Note() {
           </Stack>
         </Col>
       </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
   );
 }
