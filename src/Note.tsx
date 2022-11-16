68b936398c482d691766f50a816e6ca14eacdf89
Rohit Tokala <rtokala@purdue.edu>
2022-11-15 19:15:05 -0500

    Add show notes page

---
+++ src/Note.tsx
@@ -0,0 +1,37 @@
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useNote } from "./NoteLayout";
+
+export function Note() {
+  const note = useNote();
+
+  return (
+    <>
+      <Row className="align-itmes-center mb-4">
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
+              {note.tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button variant="outline-danger">Delete</Button>
+            <Link to="/">
+              <Button variant="outline-secondary">Back</Button>
+            </Link>
+          </Stack>
+        </Col>
+      </Row>
+    </>
+  );
+}
